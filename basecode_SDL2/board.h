0de474e980862918dbbca20a23376a8232bda3f5
Cédric MARTY <c.m2002@hotmail.fr>
2022-01-11 14:07:18 +0100

    board update

--- basecode_SDL2/board.h
+++ basecode_SDL2/board.h
@@ -7,11 +7,13 @@ typedef enum Pawn_e
 
 typedef struct Board_s
 {
-	Pawn grid[8][8];
-	Pawn gain[8][8]
+	Pawn grid[10][10];
+	Pawn gain[8][8];
 }Board;
 
+Board* Board_Init();
 int Board_addPawn(Board* board, int x, int y, Pawn pawn);
 int Board_changeColor(Board* board, int x, int y);
 Pawn Board_getColor(Board* board, int x, int y);
+void Board_setGain(Board* board, int value);
 int Board_checkGain(Board* board, Pawn colorToCheck);
