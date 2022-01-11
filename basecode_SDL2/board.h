d934800acac34408c35905f0695e50d1c8ccb3d9
Guénaël LE ROUX <gleroux@et.esiea.fr>
2022-01-11 23:07:19 +0100

    ,nkj<hfjlklsehgijfwsghfjlkqsdfkljzqwhriuhgqzelkrh;kwdshrffliqsrhioqzhtrmkqjserhflikuhqze:krjhqsdlidfuhhqzlkejnfliqsdsuhflkqezrejhlieuefhgisdufhghiqsiuuhcglisdfuhgmkisdfuhbpizreaug

--- basecode_SDL2/board.h
+++ basecode_SDL2/board.h
@@ -17,7 +17,7 @@ int Board_addPawn(Board* board, int x, int y, Pawn pawn);
 int Board_changeColor(Board* board, int x, int y);
 Pawn Board_getColor(Board* board, int x, int y);
 void Board_setGain(Board* board, int value);
-void Board_checkGain(Board* board, Pawn colorToCheck);
+int Board_checkGain(Board* board, Pawn colorToCheck);
 
 void Board_printGain(Board* board);
 int Board_changeGain(Board* board, int x, int y, int x_d, int y_d, Pawn color);
