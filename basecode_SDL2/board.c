3f04b4494015df33147813ee033d89f9b769af75
Cédric MARTY <c.m2002@hotmail.fr>
2022-01-11 12:04:50 +0100

    rerepar

--- basecode_SDL2/board.c
+++ basecode_SDL2/board.c
@@ -9,9 +9,16 @@ int Board_addPawn(Board* board, int x, int y, Pawn pawn)
 }
 int Board_changeColor(Board* board, int x, int y)
 {
+	if (board->grid[x][y])
+		return 1;
+	if (board->grid[x][y] == BLACK)
+		board->grid[x][y] = WHITE;
+	else
+		board->grid[x][y] = BLACK;
+	
 	return 0;
 }
-int Board_checkGain(Board* board)
+int Board_checkGain(Board* board, Pawn colorToCheck)
 {
 	return 0;
 }
