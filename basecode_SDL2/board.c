a57f0cb33917b41759dc60f5682afb7d5841277d
Cédric MARTY <c.m2002@hotmail.fr>
2022-01-11 13:34:34 +0100

    Board_getColor

--- basecode_SDL2/board.c
+++ basecode_SDL2/board.c
@@ -7,6 +7,10 @@ int Board_addPawn(Board* board, int x, int y, Pawn pawn)
 	board->grid[x][y] = pawn;
 	return 0;
 }
+Pawn Board_getColor(Board* board, int x, int y)
+{
+	return board->grid[x][y];
+}
 int Board_changeColor(Board* board, int x, int y)
 {
 	if (board->grid[x][y])
@@ -20,5 +24,28 @@ int Board_changeColor(Board* board, int x, int y)
 }
 int Board_checkGain(Board* board, Pawn colorToCheck)
 {
+	for (int x = 0; x < 8; ++x)
+		for (int y = 0; y < 8; y++)
+			board->gain[x][y] = 0;
+	for (int x = 0; x < 8; ++x)
+		for (int y = 0; y < 8; y++)
+		{
+			if (board->gain[x][y] == colorToCheck)
+			{
+				char directions = 0b11111111;				//012
+				if (y == 0)									//7x3
+					directions = directions & 0b00011111;	//654
+				if (y == 7)
+					directions = directions & 0b11110001;
+				if (x == 0)
+					directions = directions & 0b01111100;
+				if (x == 7)
+					directions = directions & 0b11000111;
+
+
+				
+
+			}
+		}
 	return 0;
 }
--- basecode_SDL2/board.h
+++ basecode_SDL2/board.h
@@ -11,6 +11,7 @@ typedef struct Board_s
 	Pawn gain[8][8]
 }Board;
 
-int Board_addPawn(Board* board, Pawn pawn);
+int Board_addPawn(Board* board, int x, int y, Pawn pawn);
 int Board_changeColor(Board* board, int x, int y);
+Pawn Board_getColor(Board* board, int x, int y);
 int Board_checkGain(Board* board, Pawn colorToCheck);
