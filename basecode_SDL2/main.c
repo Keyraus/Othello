d0acf42b2481188a94f0c89959fe4109aa615d91
Guénaël LE ROUX <gleroux@et.esiea.fr>
2022-01-14 15:18:55 +0100

    coucou

--- basecode_SDL2/main.c
+++ basecode_SDL2/main.c
@@ -16,9 +16,9 @@ int main(int argc, char** argv)
 
 	SDL_Texture* texture[6] = {NULL};
 	SDL_Surface* image[6] = {
-		IMG_Load("images/board.png"),
+		IMG_Load("images/board.jpg"),
 		IMG_Load("images/black.png"),
-		IMG_Load("images/white.png"),
+		IMG_Load("images/white.jpg"),
 		IMG_Load("images/num.png"),
 		IMG_Load("images/num.png"),
 		IMG_Load("images/winner.png"),
@@ -29,6 +29,11 @@ int main(int argc, char** argv)
 
 	SDL_Rect numrect;
 	SDL_Rect numpos;
+
+	SDL_Rect background = {
+		0,0,
+		800,800		
+	};
 
 	numrect.w = 500;
 	numrect.h = 500;
@@ -42,8 +47,8 @@ int main(int argc, char** argv)
 
 
 
-	pos2.w = 100;
-	pos2.h = 100;
+	pos2.w = 105;
+	pos2.h = 105;
 	pos.h = 100;
 	pos.w = 100;
 
@@ -69,7 +74,7 @@ int main(int argc, char** argv)
 
 	window = SDL_CreateWindow("Ma fenetre SDL",
 		SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, 
-		800, 800, 
+		1000, 800, 
 		SDL_WINDOW_SHOWN
 	);
 
@@ -87,7 +92,7 @@ int main(int argc, char** argv)
 	}
 		
 
-	SDL_SetRenderDrawColor(renderer, 40, 110, 0, 255);
+	SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
 
 	int lastcolor = BLACK;
 	Pawn winnerColor = 0;
@@ -104,7 +109,7 @@ int main(int argc, char** argv)
 				quit = 1;
 				break;
 			case SDL_MOUSEBUTTONUP:
-				if (Bot_doAFlip(board, WHITE, 3)) {
+				if (Bot_doAFlip(board, WHITE, 5)) {
 					flaaaaaaaaaag = 1;
 					printf("né nififnit\n");
 					winnerColor = Board_countPieces(board, &pawns[WHITE], &pawns[BLACK], &pawns[NONE]);
@@ -115,54 +120,20 @@ int main(int argc, char** argv)
 						printf("Egalite\n");
 				}
 				break;
-			case SDL_MOUSEBUTTONDOWN:
+			case SDL_MOUSEBUTTONDOWN:/*
 				SDL_GetMouseState(&pos.x, &pos.y);
 				pos.x /= 100;
 				pos.y /= 100;
-
-				
-					if (Board_addPawn(board, pos.x, pos.y, BLACK)==WHITE) {
-						
-						
-						//if (!Board_checkGain(board, lastcolor)) {
-						//	//lastcolor = lastcolor % 2 + 1;
-						//	if (!Board_checkGain(board, lastcolor)) {
-						//		flaaaaaaaaaag = 1;
-						//		printf("né nififnit\n");
-						//		winnerColor = Board_countPieces(board, &pawns[WHITE], &pawns[BLACK], &pawns[NONE]);
-						//		pawns[winnerColor] = pawns[winnerColor] + pawns[NONE];
-						//		if (winnerColor != NONE)
-						//			printf("Le gagnant gagne avec : %d points\n", pawns[winnerColor]);
-						//		else
-						//			printf("Egalite\n");
-						//	}
-						//}
-						
-
-					}
-						
-				
-					
-				
+				Board_addPawn(board, pos.x, pos.y, BLACK);*/
+				while (!Bot_doAFlip(board, BLACK, 6))
+					Bot_doAFlip(board, WHITE, 6);
 				break;
 
 			case SDL_KEYDOWN:
 				switch (e.key.keysym.sym)
 				{
-				
-
 				case SDLK_ESCAPE:
 					quit = 1;
-					break;
-
-				case SDLK_DOWN:
-					
-					break;
-
-				case SDLK_RIGHT:
-					break;
-
-				case SDLK_LEFT:
 					break;
 				}
 				break;
@@ -175,10 +146,11 @@ int main(int argc, char** argv)
 		SDL_RenderClear(renderer);
 
 		// background
-		SDL_RenderCopy(renderer, texture[0], NULL, NULL);
+
+		SDL_RenderCopy(renderer, texture[0], NULL, &background);
 		SDL_GetMouseState(&pos2.x, &pos2.y);
-		pos2.x = pos2.x / 100 * 100;
-		pos2.y = pos2.y / 100 * 100;
+		pos2.x = (pos2.x) / 100 * 100 ;
+		pos2.y = (pos2.y)/ 100 * 100 ;
 		// pyon ki flot avek la souri
 		SDL_RenderCopy(renderer, texture[lastcolor], NULL, &pos2);
 		Board_render(board, renderer, texture, winnerColor, pawns,flaaaaaaaaaag);
